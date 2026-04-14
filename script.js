cda1ad6c1fa810f530f0424c4b7ed7f6a4124885
kumkum05-prog <kumkumsinghal2211@gmail.com>
2026-04-14 17:55:30 +0530

    adding footer cart and menu

--- script.js
+++ script.js
@@ -1,6 +1,51 @@
 var swiper = new Swiper(".mySwiper", {
-      navigation: {
-        nextEl: "#next",
-        prevEl: "#prev",
-      },
-    });
+  navigation: {
+    nextEl: "#next",
+    prevEl: "#prev",
+  },
+});
+
+const cartIcon = document.querySelector('.cart-icon');
+const cartTab = document.querySelector('.cart-tab');
+const closeBtn = document.querySelector('.close-btn');
+const cardList = document.querySelector('.card-list');
+
+cartIcon.addEventListener('click', () => {
+  cartTab.classList.add('cart-tab-active');
+})
+
+closeBtn.addEventListener('click', () => {
+  cartTab.classList.remove('cart-tab-active')
+})
+
+let productList = [];
+
+const showCard = () => {
+  productList.forEach(product  => {
+    
+    const orderCard= document.createElement('div');
+    orderCard.classList.add('order-card');
+    orderCard.innerHTML=`
+                          <div class="card-image">
+                            <img src="${product.image}" />
+                          </div>
+
+                          <h4>${product.name}</h4>
+                          <h4 class="price">${product.price}</h4>
+                          <a href="#" class="btn">Add to cart</a>
+                        `
+    
+
+    cardList.appendChild(orderCard);                    
+  })
+}
+
+const initApp = () => {
+  fetch('products.json').then
+    (response => response.json()).then
+    (data => {
+      productList = data;
+      showCard();
+    })
+}
+initApp();
